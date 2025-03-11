0787c4631c4d3441de90c4689b755461add0fe42
sumonDev9 <sumonmit678@gmail.com>
2025-03-11 21:17:49 +0530

    Add responsive chat UI with Tailwind CSS

--- index.js
+++ index.js
@@ -7,17 +7,25 @@ const expressServer = http.createServer(app);
 const {Server} = require('socket.io');
 const io = new Server(expressServer);
 
+// Serve the index.html file
 app.get('/', (req, res) => {
     res.sendFile(__dirname+"/index.html")
 })
 
+// Handle socket connections
 io.on('connection', (socket) => {
     console.log('a user connected');
+
+
+       // Handle disconnection
+       socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
   });
 
 
 
-
+// Start the server
 expressServer.listen(3000, () => {
     console.log('Server runing')
 })
